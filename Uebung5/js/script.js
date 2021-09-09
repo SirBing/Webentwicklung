e3603786c98b5ec98399616dc200a331c216b7a6
moritzmeyer123 <moritzmeyer@Moritzs-MacBook-Pro.fritz.box>
2021-09-09 12:51:39 +0200

    JQuery Start

---
+++ Uebung5/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#toggle_button").click(function () { 
+        $("#paragraph").toggle();
+        
+    });
+}); 
+
+/*var outputtext = document.getElementById('paragraph');
+
+function f(){
+    if(outputtext.style.display === "none") {
+        outputtext.style.display = "block";
+    } else {
+        outputtext.style.display = "none";
+    }
+} */
+
